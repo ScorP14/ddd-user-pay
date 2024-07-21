4e02d6a624c199d6a4fdeee2f97cc3e67c32b9eb
ScorP14 <mail@mail.com>
2024-07-21 13:34:08 +0800

    refactoring

--- src/models/user/entyty.py
+++ src/models/user/entyty.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-from src.models.common.exeption import BaseError
 from src.models.common.value_object import ItemID
 from src.models.user.value_object import UserName, UserPassword, UserEmail
 
@@ -12,34 +11,6 @@ class User:
     last_name: UserName
     email: UserEmail
     password: UserPassword
-
-
-@dataclass
-class Payment:
-    id: ItemID
-    user: User
-    amount: float
-
-
-@dataclass
-class Account:
-    id: ItemID
-    user: User
-    balance: float
-
-
-
-
-
-user1 = User(
-    id=ItemID(),
-    first_name=UserName('Fsd'), last_name=UserName('Last'),
-    email=UserEmail('Mail@wqe'), password=UserPassword('SECRET12')
-)
-
-
-print(user1)
-print(user1.password == 'SECRET123')
 
 
 @dataclass
